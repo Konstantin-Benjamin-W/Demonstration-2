30c21752b0bdd6538b3061eede7e746c2ffb4e24
Oleksii.S <syromolotov.oleksii@gmail.com>
2020-02-06 23:22:56 +0200

    Added few tests for ArrayImpl.java

--- test/ArrayImplTest.java
+++ test/ArrayImplTest.java
@@ -1,5 +1,58 @@
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ArrayImplTest {
 
+    private  ArrayImpl array;
+
+    @Before
+    public void createArrayImpl(){
+        array = new ArrayImpl();
+    }
+
+    @Test
+    public void shouldAddThreeElementToArrayAndPrint(){
+        array.add("A");
+        array.add("B");
+        array.add("C");
+
+        assertEquals(3, array.size());
+
+        assertEquals("[A, B, C]", array.toString());
+    }
+
+    @Test
+    public void shouldAddFourElementToArrayAndPrint(){
+        array.add("A");
+        array.add("B");
+        array.add("C");
+        array.add("D");
+        assertEquals(4, array.size());
+
+        assertEquals("[A, B, C, D]", array.toString());
+    }
+
+    @Test
+    public void shouldAddFiveElementToArray(){
+        array.add("A");
+        array.add("B");
+        array.add("C");
+        array.add("D");
+        array.add("E");
+        assertEquals(5, array.size());
+    }
+
+    @Test
+    public void shouldAddFiveElementAndRemoveAtNullIndex(){
+        array.add("A");
+        array.add("B");
+        array.add("C");
+        array.add("D");
+        array.add("E");
+        assertEquals(5, array.size());
+        array.remove(0);
+        assertEquals(array.get(0), "B");
+    }
 }
