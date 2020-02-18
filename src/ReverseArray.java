76d1270e079b0b4e61837f705cfcef136be899e6
Konstantin-Benjamin-W <konstantin.bwb@gmail.com>
2020-02-18 16:42:16 +0200

    Second Project Sub-task

---
+++ src/ReverseArray.java
@@ -0,0 +1,8 @@
+public class ReverseArray {
+    public static void main(String [] args){
+        int j = 0;
+        for (int i = array.length; i > -1; i--){
+            array[i] = array[j]
+        }
+    }
+}
